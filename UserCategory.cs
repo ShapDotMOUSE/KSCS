002a828a94cf582c7b09aa09fed32c159f04c918
Jansoon <ckdals4600@naver.com>
2023-05-14 14:31:54 +0900

    feat: 카테고리 유저 컨트롤 추가
    - 카테고리 추가로 컨트롤 생성 시 텍스트 박스 포커스, 사용자로부터 이름을 입력 받음
    - 이름은 최대 4글자까지 입력 가능
    - 엔터 클릭 시 입력된 내용을 이름으로하는 label Visible와 함께 TextBox InVisible
    - 카테고리 이름 더블클릭 시 이름 수정 가능

---
+++ UserCategory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSCS
+{
+    public partial class UserCategory : UserControl
+    {
+        public UserCategory()
+        {
+            InitializeComponent();
+        }
+
+        public void SetlblCategory(string categoryName)
+        {
+            lblCategory.Text = categoryName;
+        }
+
+
+        private void txtCategory_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (txtCategory.Text.Length > 0)
+                {
+                    lblCategory.Text = txtCategory.Text;
+                    txtCategory.Visible= false;
+                }
+            }
+        }
+
+        private void UserCategory_Load(object sender, EventArgs e)
+        {
+            txtCategory.MaxLength = 4;
+            this.ActiveControl= txtCategory;
+            txtCategory.Focus();
+        }
+
+        private void lblCategory_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            txtCategory.Visible = true;
+            txtCategory.Focus();
+        }
+    }
+}

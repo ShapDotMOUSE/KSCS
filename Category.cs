298720d74aefd34d4e084fba85c7c18b452bd3c6
Jansoon <ckdals4600@naver.com>
2023-05-16 23:09:44 +0900

    feat: Add Category Class
    - 카테고리 관리 클래스 추가
    - 상위 카테고리와 하위 카테고리 각각 hashTable 관리

---
+++ Category.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSCS
+{
+    internal class Category
+    {
+        public Hashtable MainCategory = new Hashtable();
+        public Hashtable Subdivision = new Hashtable();
+
+        public void AddSubdivision(string Main, string Sub)
+        {
+            HashSet<string> SubdivisionSet = MainCategory[Main] as HashSet<string>;
+            SubdivisionSet.Add(Sub);
+            MainCategory[Main] = SubdivisionSet;
+            Subdivision[Sub] = Main;
+        }
+
+        public HashSet<string> GetSubdivisionSetByMainCategory(string Main)
+        {
+            return MainCategory[Main] as HashSet<string>;
+        }
+
+        public string GetMainCategoryBySubdivision(string Sub)
+        {
+            return Subdivision[Sub] as string;
+        }
+    }
+}

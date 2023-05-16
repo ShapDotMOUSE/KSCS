e9113bab51b4cb81f8fd36bfac1139781026cfd3
Jansoon <ckdals4600@naver.com>
2023-05-17 03:19:32 +0900

    Test: 테스트용 생성자 추가

--- Category.cs
+++ Category.cs
@@ -7,11 +7,46 @@ using System.Threading.Tasks;
 
 namespace KSCS
 {
-    internal class Category
+    public class Category
     {
         public Hashtable MainCategory = new Hashtable();
         public Hashtable Subdivision = new Hashtable();
 
+        public Category() { 
+            HashSet<string> SchoolCategories = new HashSet<string>();
+            SchoolCategories.Add("학사일정");
+            SchoolCategories.Add("과제");
+            SchoolCategories.Add("퀴즈");
+            SchoolCategories.Add("온라인 강의");
+            HashSet<string> PersonalCategories = new HashSet<string>();
+            PersonalCategories.Add("생일");
+            PersonalCategories.Add("약속");
+            PersonalCategories.Add("식사");
+            HashSet<string> EtcCatergories = new HashSet<string>();
+            EtcCatergories.Add("기타1");
+            EtcCatergories.Add("기타2");
+            EtcCatergories.Add("기타3");
+            EtcCatergories.Add("기타4");
+            EtcCatergories.Add("기타5");
+
+            MainCategory["ShcoolCategory"] = SchoolCategories;
+            MainCategory["PersonalCategory"] = PersonalCategories;
+            MainCategory["EtcCategory"] = EtcCatergories;
+
+            Subdivision["학사일정"] = "ShcoolCategory";
+            Subdivision["과제"] = "ShcoolCategory";
+            Subdivision["퀴즈"] = "ShcoolCategory";
+            Subdivision["온라인 강의"] = "ShcoolCategory";
+
+            Subdivision["생일"] = "PersonalCategory";
+            Subdivision["약속"] = "PersonalCategory";
+            Subdivision["식사"] = "PersonalCategory";
+
+            Subdivision["기타1"] = "EtcCategory";
+            Subdivision["기타2"] = "EtcCategory";
+            Subdivision["기타3"] = "EtcCategory";
+
+        }
         public void AddSubdivision(string Main, string Sub)
         {
             HashSet<string> SubdivisionSet = MainCategory[Main] as HashSet<string>;

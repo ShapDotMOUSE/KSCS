b57963169b9b82abeca0372bcbdf187b3ad82b0b
Jansoon <ckdals4600@naver.com>
2023-05-19 00:31:27 +0900

    feat: 탭 관리 자료구조 변경
    - 현재 탭에 따른 체크 카테고리만 관리하여 업데이트 하던 구조에서 전체 탭에 대한 체크 카테고리를 관리하도록 변경

--- Category.cs
+++ Category.cs
@@ -9,9 +9,9 @@ namespace KSCS
 {
     public class Category
     {
-        public Hashtable ParentCategory = new Hashtable();
-        public Hashtable SubCategory = new Hashtable();
-        public HashSet<string> Checked = new HashSet<string>();
+        public Hashtable ParentCategorys = new Hashtable();
+        public Hashtable SubCategorys = new Hashtable();
+        public Hashtable Tabs = new Hashtable();
 
         public void TestCategory()
         {
@@ -29,116 +29,112 @@ namespace KSCS
             EtcCatergories.Add("기타2");
             EtcCatergories.Add("기타3");
 
-            ParentCategory["SchoolCategory"] = SchoolCategories;
-            ParentCategory["PersonalCategory"] = PersonalCategories;
-            ParentCategory["EtcCategory"] = EtcCatergories;
+            ParentCategorys["SchoolCategory"] = SchoolCategories;
+            ParentCategorys["PersonalCategory"] = PersonalCategories;
+            ParentCategorys["EtcCategory"] = EtcCatergories;
 
-            SubCategory["학사일정"] = "SchoolCategory";
-            SubCategory["과제"] = "SchoolCategory";
-            SubCategory["퀴즈"] = "SchoolCategory";
-            SubCategory["온라인 강의"] = "SchoolCategory";
+            SubCategorys["학사일정"] = "SchoolCategory";
+            SubCategorys["과제"] = "SchoolCategory";
+            SubCategorys["퀴즈"] = "SchoolCategory";
+            SubCategorys["온라인 강의"] = "SchoolCategory";
 
-            SubCategory["생일"] = "PersonalCategory";
-            SubCategory["약속"] = "PersonalCategory";
-            SubCategory["식사"] = "PersonalCategory";
+            SubCategorys["생일"] = "PersonalCategory";
+            SubCategorys["약속"] = "PersonalCategory";
+            SubCategorys["식사"] = "PersonalCategory";
 
-            SubCategory["기타1"] = "EtcCategory";
-            SubCategory["기타2"] = "EtcCategory";
-            SubCategory["기타3"] = "EtcCategory";
+            SubCategorys["기타1"] = "EtcCategory";
+            SubCategorys["기타2"] = "EtcCategory";
+            SubCategorys["기타3"] = "EtcCategory";
 
-            Checked.Add("학사일정");
-            Checked.Add("과제");
-            Checked.Add("퀴즈");
-            Checked.Add("온라인강의");
-            Checked.Add("생일");
-            Checked.Add("약속");
-            Checked.Add("식사");
-            Checked.Add("기타1");
-            Checked.Add("기타2");
-            Checked.Add("기타3");
-        }
+            HashSet<String> TabCategory1 = new HashSet<String>();
+            TabCategory1.Add("학사일정");
+            TabCategory1.Add("온라인강의");
+            TabCategory1.Add("약속");
+            TabCategory1.Add("기타1");
+            Tabs["btnTestTab1"] = TabCategory1;
 
-        public void TestTab1()
-        {
-            Checked.Clear();
-            Checked.Add("학사일정");
-            Checked.Add("온라인강의");
-            Checked.Add("약속");
-            Checked.Add("기타1");
-        }
+            HashSet<String> TabCategory2 = new HashSet<String>();
+            TabCategory2.Add("학사일정");
+            TabCategory2.Add("과제");
+            TabCategory2.Add("퀴즈");
+            TabCategory2.Add("온라인강의");
+            TabCategory2.Add("기타1");
+            TabCategory2.Add("기타2");
+            Tabs["btnTestTab2"] = TabCategory2;
 
-        public void TestTab2()
-        {
-            Checked.Clear();
-            Checked.Add("학사일정");
-            Checked.Add("과제");
-            Checked.Add("퀴즈");
-            Checked.Add("온라인강의");
-            Checked.Add("기타1");
-            Checked.Add("기타2");
-        }
-
-        public void TestTab3()
-        {
-            Checked.Clear();
-            Checked.Add("학사일정");
-            Checked.Add("과제");
-            Checked.Add("퀴즈");
-            Checked.Add("온라인강의");
-            Checked.Add("생일");
-            Checked.Add("약속");
-            Checked.Add("식사");
-            Checked.Add("기타1");
-            Checked.Add("기타2");
-            Checked.Add("기타3");
+            HashSet<String> TabCategory3 = new HashSet<String>();
+            TabCategory3.Add("학사일정");
+            TabCategory3.Add("과제");
+            TabCategory3.Add("퀴즈");
+            TabCategory3.Add("온라인강의");
+            TabCategory3.Add("생일");
+            TabCategory3.Add("약속");
+            TabCategory3.Add("식사");
+            TabCategory3.Add("기타1");
+            TabCategory3.Add("기타2");
+            TabCategory3.Add("기타3");
+            Tabs["btnTestTab3"] = TabCategory3;
         }
 
         public void AddSubdivision(string Main, string Sub)
         {
-            HashSet<string> SubdivisionSet = ParentCategory[Main] as HashSet<string>;
+            HashSet<string> SubdivisionSet = ParentCategorys[Main] as HashSet<string>;
             SubdivisionSet.Add(Sub);
-            ParentCategory[Main] = SubdivisionSet;
-            SubCategory[Sub] = Main;
+            SubCategorys[Sub] = Main;
         }
 
         public void ChageSubdivisionName(string Old, string New)
         {
-            string ParentCategory = KSCS.Category.SubCategory[Old] as string;
+            string ParentCategory = KSCS.Category.SubCategorys[Old] as string;
             //대분류단에서 이름 수정
-            HashSet<string> Set = KSCS.Category.ParentCategory[ParentCategory] as HashSet<string>;
+            HashSet<string> Set = KSCS.Category.ParentCategorys[ParentCategory] as HashSet<string>;
             Set.Remove(Old);
             Set.Add(New);
-            KSCS.Category.ParentCategory[ParentCategory] = Set;
             //하위 카테고리 단에서의 수정
-            KSCS.Category.SubCategory.Remove(Old);
-            KSCS.Category.SubCategory.Add(New, ParentCategory);
+            KSCS.Category.SubCategorys.Remove(Old);
+            KSCS.Category.SubCategorys.Add(New, ParentCategory);
+
+            foreach(DictionaryEntry tab in Tabs)
+            {
+                HashSet<string> tabCategorys = tab.Value as HashSet<string>;
+                if (tabCategorys.Contains(Old))
+                {
+                    tabCategorys.Remove(Old);
+                    tabCategorys.Add(New);
+                }
+            }
         }
 
         public void ChangeParentOfSub(string NewMain, string Sub)
         {
-            string sourcsMain = SubCategory[Sub] as string;
+            string sourcsMain = SubCategorys[Sub] as string;
             //기존 main에서 sub 제거
-            HashSet<string> deleteSubformSourceMain = ParentCategory[sourcsMain] as HashSet<string>;
+            HashSet<string> deleteSubformSourceMain = ParentCategorys[sourcsMain] as HashSet<string>;
             deleteSubformSourceMain.Remove(Sub);
-            ParentCategory[sourcsMain] = deleteSubformSourceMain;
             //sub에 대한 main 변경
-            SubCategory[Sub] = NewMain;
+            SubCategorys[Sub] = NewMain;
             //새로운 main에 sub 추가
-            HashSet<string> AddSubIntoNewParent = ParentCategory[NewMain] as HashSet<string>;
+            HashSet<string> AddSubIntoNewParent = ParentCategorys[NewMain] as HashSet<string>;
             AddSubIntoNewParent.Add(Sub);
-            ParentCategory[NewMain] = AddSubIntoNewParent;
         }
 
-        public void AddChecked(string Sub)
+        public bool IsChecked(string Tab, string Sub)
         {
-            Checked.Add(Sub);
+            HashSet<string> TabCategory = Tabs[Tab] as HashSet<string>;
+            return TabCategory.Contains(Sub);
+        }
+        public void AddChecked(string Tab, string Sub)
+        {
+            HashSet<string> TabCategory = Tabs[Tab] as HashSet<string>;
+            TabCategory.Add(Sub);
         }
 
-        public void DeletChecked(string Sub)
+        public void DeletChecked(string Tab, string Sub)
         {
-            if (Checked.Contains(Sub))
+            HashSet<string> TabCategory = Tabs[Tab] as HashSet<string>;
+            if (TabCategory.Contains(Sub))
             {
-                Checked.Remove(Sub);
+                TabCategory.Remove(Sub);
             }
         }
     }

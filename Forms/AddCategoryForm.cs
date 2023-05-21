201776538b9cccc86da083d59fd169e773aa514d
Seong-Myeong <mg0454@naver.com>
2023-05-22 02:57:48 +0900

    design : 카테고리 폼 추가

---
+++ Forms/AddCategoryForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSCS.Forms
+{
+    public partial class AddCategoryForm : Form
+    {
+        public AddCategoryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void AddCategoryForm_ResizeBegin(object sender, EventArgs e)
+        {
+            this.Size = new Size(280, 240);
+        }
+    }
+}

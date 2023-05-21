0df561f995585d4538bc11c96a221b74ffbdae60
jansoon <ckdals4600@naver.com>
2023-05-21 14:29:53 +0900

    fix: 크로스 쓰레드 작업 오류 - 크로스 쓰레드 작업에 대해 대리자 사용

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -309,6 +309,8 @@ namespace KSCS
                 };
                 lbl.Location = new Point(0, index * (lbl.Height + 3));
                 panelMagam.Controls.Add(lbl);
+                //if (panel.InvokeRequired) panelMagam.Invoke(new MethodInvoker(delegate { panelMagam.Controls.Add(lbl); }));
+                //else panelMagam.Controls.Add(lbl);
                 index++;
             }
         }

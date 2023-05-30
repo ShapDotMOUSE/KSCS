b11c76d6d7c7b56ca54add79e1de63216d317011
jansoon <ckdals4600@naver.com>
2023-05-30 15:57:29 +0900

    feat: 카테고리 수정 연결 및 DB 연결

--- UserControls/MainForm/UserSubCategory.cs
+++ UserControls/MainForm/UserSubCategory.cs
@@ -41,6 +41,11 @@ namespace KSCS
            chkCategory.Checked = check;
         }
 
+        public void SetCheckedEnable(bool enable)
+        {
+            chkCategory.Enabled = enable;
+        }
+
         public void SetBasicMode(string txt)
         {
             this.Name = txt;
@@ -50,6 +55,11 @@ namespace KSCS
 
         private void UserCategory_Load(object sender, EventArgs e)
         {
+            if (KSCS_static.TabName == "All")
+            {
+                chkCategory.Enabled = false;
+                chkCategory.Checked = true;
+            }
             txtCategory.MaxLength = 4;
             this.ActiveControl = txtCategory;
             txtCategory.Focus();
@@ -72,6 +82,7 @@ namespace KSCS
                     {
                         //신규 카테고리인 경우
                         KSCS_static.category.AddSubdivision(MainCategory, txtCategory.Text);
+                        Database.CreateSubCategory(MainCategory, txtCategory.Text);
                     }
                     lblCategory.Text = txtCategory.Text;
                     this.Name = txtCategory.Text;
@@ -131,7 +142,8 @@ namespace KSCS
         //유저 카테고리 수정 폼 로드
         private void UserCategory_DoubleClick(object sender, EventArgs e)
         {
-           TempCategorySetting temp = new TempCategorySetting((FlowLayoutPanel)((FlowLayoutPanel)this.Parent).Parent, ((FlowLayoutPanel)this.Parent).Name , lblCategory.Text);
+            UserMainCategory parent = (UserMainCategory)((FlowLayoutPanel)this.Parent).Parent;
+           AddCategoryForm temp = new AddCategoryForm((FlowLayoutPanel)(parent.Parent), parent.Name, lblCategory.Text);
            temp.ShowDialog();
         }
 

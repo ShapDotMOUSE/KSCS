c49c868b25cc8a22db5956616e52645615238820
jansoon <ckdals4600@naver.com>
2023-06-01 10:09:48 +0900

    feat: KLAS 하위 카테고리 상위 카테고리 변경 불가

--- Forms/AddCategoryForm.cs
+++ Forms/AddCategoryForm.cs
@@ -1,4 +1,5 @@
 ﻿using KSCS.Class;
+using KSCS.UserControls.MainForm;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -56,22 +57,30 @@ namespace KSCS.Forms
         {
             if (originMain != cmbMain.SelectedItem.ToString())
             {
-                string NewMain = cmbMain.SelectedItem.ToString();
-                //변경 사항 Category에 적용
-                category.ChangeParentOfSub(originMain, NewMain, originSub);
-                //변경 사항 MainForm에 적용
-                UserMainCategory NewParent = MainCategory.Controls[NewMain] as UserMainCategory;
-                UserMainCategory OldParent = MainCategory.Controls[originMain] as UserMainCategory;
-                UserSubCategory OldUc = ((FlowLayoutPanel)((OldParent).Controls["flpSubCategory"]))
-                                        .Controls[originSub] as UserSubCategory;
-                UserSubCategory NewUc = new UserSubCategory();
-                NewUc.SetBasicMode(originSub);
-                NewParent.Controls["flpSubCategory"].Controls.Add(NewUc);
-                OldParent.Controls["flpSubCategory"].Controls.Remove(OldUc);
+                if (originMain == "KLAS")
+                {
+                    MessageBox.Show("해당 카테고리는 상위 카테고리를 변경할 수 없습니다");
+                    cmbMain.SelectedItem = originMain;
+                }
+                else
+                {
+                    string NewMain = cmbMain.SelectedItem.ToString();
+                    //변경 사항 Category에 적용
+                    category.ChangeParentOfSub(originMain, NewMain, originSub);
+                    //변경 사항 MainForm에 적용
+                    UserMainCategory NewParent = MainCategory.Controls[NewMain] as UserMainCategory;
+                    UserMainCategory OldParent = MainCategory.Controls[originMain] as UserMainCategory;
+                    UserSubCategory OldUc = ((FlowLayoutPanel)((OldParent).Controls["flpSubCategory"]))
+                                            .Controls[originSub] as UserSubCategory;
+                    UserSubCategory NewUc = new UserSubCategory();
+                    NewUc.SetBasicMode(originSub);
+                    NewParent.Controls["flpSubCategory"].Controls.Add(NewUc);
+                    OldParent.Controls["flpSubCategory"].Controls.Remove(OldUc);
+                    //DB 업데이트
+                    Database.UpdateParentCategoryOfSubCategory(NewMain, originSub);
 
-                Database.UpdateParentCategoryOfSubCategory(NewMain, originSub);
-
-                originMain = NewMain;
+                    originMain = NewMain;
+                }
             }
             if (originSub != txtboxSub.Text)
             {
@@ -80,23 +89,29 @@ namespace KSCS.Forms
                 category.ChageSubdivisionName(originMain, originSub, NewSub);
                 FlowLayoutPanel Parent = MainCategory.Controls[originMain].Controls["flpSubCategory"] as FlowLayoutPanel;
                 ((UserSubCategory)Parent.Controls[originSub]).SetBasicMode(NewSub);
-
+                //DB 업데이트
                 Database.UpdateSubCategory(NewSub, originSub);
+                //라벨 수정
+                ((UserLabel)MainForm.flowLayoutPanelLable.Controls["label" + originSub]).SetName(NewSub);
                 originSub = NewSub;
             }
 
             if(originColor != boxColor.FillColor)
             {
+                //카테고리에 수정 내용 반영
                 ((UserSubCategory)(MainCategory.Controls[originMain].Controls["flpSubCategory"].Controls[originSub])).SetColor(boxColor.FillColor);
-                KSCS_static.category.SetColor(originSub, boxColor.FillColor);
+                category.SetColor(originSub, boxColor.FillColor);
+                //DB 업데이트
                 Database.UpdateSubCategoryColor(originSub, boxColor.FillColor);
+                //라벨 수정
+                ((UserLabel)MainForm.flowLayoutPanelLable.Controls["label" + originSub]).SetColor(boxColor.FillColor);
             }
             Close();
 
         }
     
 
-    private void guna2Button2_Click(object sender, EventArgs e)
+        private void guna2Button2_Click(object sender, EventArgs e)
         {
             this.Close();
 

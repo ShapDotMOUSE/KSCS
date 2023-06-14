aca770266721356eeb6fa07b35219f6e0a171b43
goder-0 <goder0@naver.com>
2023-06-15 08:44:48 +0900

    feat: Member add 버그 수정

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -534,7 +534,7 @@ namespace KSCS
             List<string> testStdnums = new List<string>
                 {
                     "2019203082",
-                    "2019203018",
+                    "2019203055",
                 };
 
             btnUserSharingAddButton.ChangeStatus(true);
